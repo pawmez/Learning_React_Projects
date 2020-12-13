dfebdcb9bafa98ccdfd8e170e478ffedad0d3338
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-13 14:37:13 +0100

    add Loading and Tours components

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -7,8 +7,18 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
-  
-  return <h2>Tours Project Setup</h2>
+
+  if(loading){
+     return (
+     <main>
+       <Loading />
+     </main>
+     );
+  };
+   
+  return <main>
+      <Tours />
+  </main>
 }
 
 export default App

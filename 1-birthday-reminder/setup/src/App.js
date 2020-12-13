2e31d89de7a421e01e326b91d08a248a0e534258
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-13 14:02:30 +0100

    finished 1-bithday-reminder project

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [birthday, setBirthday] = React.useState(data);
+
+  return (
+  <main>
+    <section className="container">
+      <h3>{birthday.length} birthdays today</h3>
+      <List birthday={birthday}/>
+      <button onClick={()=> setBirthday([])}>clear all</button>
+    </section>
+  </main>
+  );
 }
 
 export default App;
--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import data from './data';
 
-const List = () => {
-  return (
+const List = ({birthday}) => {
+    return (
     <>
-      <h2>list component</h2>
+    {birthday.map((person) => {
+      const {id,name,age,image} = person
+     return <article key={id} className="person">
+       <img src={image} alt={name}/> 
+       <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+       </div>
+    </article>
+})}
     </>
   );
 };

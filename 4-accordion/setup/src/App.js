e08aacdc01e69efb5d2b6d3f7a5eddb8977903cf
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-14 17:04:29 +0100

    useState, wip App.js

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import data from './data';
+import Question from './Question';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+const [index, setIndex] = useState(0);
+const {id, title, info} = data[index];
+
+
+  return <main className="container">
+    <div>
+      <Question />  
+    </div>
+      
+    </main>;
 }
 
 export default App;

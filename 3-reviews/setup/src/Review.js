0fd4be974e20f8aab87bb2effa068adb17473d02
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-14 10:18:14 +0100

    add person infos, add buttons

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -9,7 +9,24 @@ const Review = () => {
   return <article className="review">
     <div className="img-container">
       <img src={image} alt={name} className="person-img"/>
+      <span className="quote-icon">
+        <FaQuoteRight />
+      </span>
     </div>
+    <h4 className="author">{name}</h4>
+    <p className="job">{job}</p>
+    <p className="info">{text}</p>
+    <div className="button-container">
+      <button className="prev-btn">
+        <FaChevronLeft />
+      </button>
+      <button className="next-btn">
+        <FaChevronRight />
+      </button>      
+    </div>
+    <button className="random-btn">
+        surprise me
+      </button>
   </article>;
 };
 

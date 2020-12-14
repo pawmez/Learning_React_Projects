088aa809f38323628824839ed5f7e331ecb4aa83
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-14 21:42:09 +0100

    add live buttons to Categories.js; finish project

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const Categories = ({categories, filterItems}) => {
-  return <div className="btn-container">
-    <button className="filter-btn" onClick={() => {filterItems('all')}}>
-      all
-    </button>
-    <button className="filter-btn" onClick={() => {filterItems('breakfast')}}>
-      breakfast
-    </button>
-    </div>;
+  return (
+  <div className="btn-container">
+    {categories.map((category, index) => {
+return (
+  <button type="button" className="filter-btn" key={index} onClick={()=> filterItems(category)}>
+  {category}
+  </button>
+    );
+    })}
+    </div>
+);
 };
 
 export default Categories;

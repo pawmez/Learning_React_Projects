6173338a88a8b14e91b380c7015d2ac815ca44b8
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-15 16:19:12 +0100

    fetch and add 1 job description

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -50,7 +50,14 @@ function App() {
           {/* job info */}
           <article className="job-info">
             <h3>{title}</h3>
-
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties.map((duty, index) => {
+              return <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            })}
           </article>
         </div>
   </section>

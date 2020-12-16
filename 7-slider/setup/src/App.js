bc01447fa01a56bc093bd46dd9cd0935ee165805
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-16 11:38:36 +0100

    data directly in useState default

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -3,16 +3,13 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  const [people, setPeople] = useState([]);
+  const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    setPeople(data)
-  },[]);
 
   const {image, name, title, quote} = people[index]
 
-  return (<main>
+  return (
+  <main>
     <h4>{name}</h4>
   </main>
   );

21faaf730c37f575a2a7142647f29b2792236bd5
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-16 13:31:19 +0100

    add nextSlide variable

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -17,8 +17,9 @@ function App() {
       {people.map((person, personIndex) => {
         const {id, image, name, title, quote} = person;
         // placeholder
+        let position = 'nextSlide'
         return (
-        <article key={id}>
+        <article className={position} key={id}>
           <img src={image} alt={name} className="person-img"/>
           <h4>{name}</h4>
           <p className="title">{title}</p>

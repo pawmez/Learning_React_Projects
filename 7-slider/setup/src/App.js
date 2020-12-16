52d2915edaad4ba371ad1c304d437a9683e37649
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-16 11:29:39 +0100

    add state components

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -3,7 +3,19 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    setPeople(data)
+  },[]);
+
+  const {image, name, title, quote} = people[index]
+
+  return (<main>
+    <h4>{name}</h4>
+  </main>
+  );
 }
 
 export default App;

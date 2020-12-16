7e03e25323d01ddaf0f5aa69ae5c8e8999edcfb8
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-16 11:46:39 +0100

    structure main section

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -4,14 +4,37 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
-  const [index, setIndex] = useState(0);
-
-  const {image, name, title, quote} = people[index]
+  const [index, setIndex] = useState(0);  
 
   return (
-  <main>
-    <h4>{name}</h4>
-  </main>
+  <section className="section">
+    <div className="title">
+      <h2>
+        <span>/</span>reviews
+      </h2>
+    </div>
+    <div className="section-center">
+      {people.map((person, personIndex) => {
+        const {id, image, name, title, quote} = person;
+        // placeholder
+        return (
+        <article key={id}>
+          <img src={image} alt={name} className="person-img"/>
+          <h4>{name}</h4>
+          <p className="title">{title}</p>
+          <p className="text">{quote}</p>
+          <FaQuoteRight className="icon"/>
+        </article>
+        );
+      })}
+      <button className="prev">
+        <FiChevronLeft />
+      </button>
+      <button className="next">
+        <FiChevronRight />
+      </button>
+    </div>    
+  </section>
   );
 }
 

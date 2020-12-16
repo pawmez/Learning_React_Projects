dce222d9956a7c1b2973afa98c7c1708e73453ee
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-16 15:58:39 +0100

    add Slides.js component; separate App.js; finish

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+import Slides from './Slides';
+
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);  
@@ -20,7 +20,7 @@ function App() {
   // Time interval
   useEffect(() => {
     let slider = setInterval(() => {
-      setIndex(addIndex(index));
+      addIndex(index);
     }, 3000);
     return () => clearInterval(slider) // important to clear!
   },[index])
@@ -30,7 +30,7 @@ function App() {
     if(number >= people.length){
       number = 0
     }
-    return number
+    return setIndex(number)
   }
 
   const subtractIndex = (number) => {
@@ -38,47 +38,18 @@ function App() {
     if(number < 0){
       number = people.length - 1
     }
-    return number
+    return setIndex(number)
   }
 
   return (
+    
   <section className="section">
-    <div className="title">
-      <h2>
-        <span>/</span>reviews
-      </h2>
-    </div>
-    <div className="section-center">
-      {people.map((person, personIndex) => {
-        const {id, image, name, title, quote} = person;
-        // By default, position slides on the right hand side
-        let position = 'nextSlide'
-        // Position in the center
-        if(personIndex === index){
-          position = 'activeSlide'
-        }
-        // Slide on the left: either with index -1 ORa if 0, the last one in array 
-        if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
-          position = 'lastSlide'
-        }
-
-        return (
-        <article className={position} key={id}>
-          <img src={image} alt={name} className="person-img"/>
-          <h4>{name}</h4>
-          <p className="title">{title}</p>
-          <p className="text">{quote}</p>
-          <FaQuoteRight className="icon"/>
-        </article>
-        );
-      })}
-      <button className="prev">
-        <FiChevronLeft onClick={()=> setIndex(subtractIndex(index))}/>
-      </button>
-      <button className="next">
-        <FiChevronRight onClick={()=> setIndex(addIndex(index))}/>
-      </button>
-    </div>    
+    <Slides 
+    people={people} 
+    index={index} 
+    addIndex={addIndex} 
+    subtractIndex={subtractIndex}
+    />     
   </section>
   );
 }

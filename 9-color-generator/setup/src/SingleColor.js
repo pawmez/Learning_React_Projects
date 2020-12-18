d85503a922484e2bedb7c577ded6990819bcf096
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-18 20:30:41 +0100

    use hexValue to get the color # name

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -4,11 +4,12 @@ import rgbToHex from './utils'
 const SingleColor = ({rgb, weight, index, hexColor}) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb);
+  //const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`
   return (
   <article className={'color'} style={{backgroundColor: `rgb(${bcg})`}}>
     <p className="percent-value">{weight}%</p>
-    <p className="color-value">{hex}</p>
+    <p className="color-value">{hexValue}</p>
   </article>
   );
 }

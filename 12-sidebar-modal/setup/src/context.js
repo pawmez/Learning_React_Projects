c5d1d0d075de9a6156505db19bc35ece70c0ffe2
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-21 21:16:30 +0100

    prepare for custom hook in context.js

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -6,5 +6,7 @@ const AppContext = React.createContext();
 const AppProvider = ({children}) => {
     return <AppContext.Provider value="hello">{children}</AppContext.Provider>
 }
+// custom hook
+
 
 export {AppContext, AppProvider}

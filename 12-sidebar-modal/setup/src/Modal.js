af37db99b509048118427f9865d1035ea034df7d
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-21 21:34:31 +0100

    add import context.js, open/close Modal functionality

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import {useGlobalContext} from './context'
 const Modal = () => {
-  return <div className={`modal-overlay`}>
+  const {isModalOpen, closeModal} = useGlobalContext();
+
+  return <div className={`modal-overlay ${isModalOpen?'show-modal':''}`}>
     <div className="modal-container">
       <h3>modal content</h3>
-      <button className="close-modal-btn">
+      <button className="close-modal-btn" onClick={closeModal}>
         <FaTimes />
       </button>
     </div>

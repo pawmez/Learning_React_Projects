71bd9d00094d5caa278e9072feb5498b4097f920
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-21 21:42:09 +0100

    add import context.js, open/close Sidebar

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -2,12 +2,16 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import {useGlobalContext} from './context'
 
 const Sidebar = () => {
-  return <aside className={`sidebar`}>
+  const {isSidebarOpen, closeSidebar} = useGlobalContext();
+
+
+  return <aside className={`sidebar ${isSidebarOpen?'show-sidebar':''}`}>
     <div className="sidebar-header">
       <img src={logo} alt="logo" alt="coding addict"/>
-      <button className="close-btn">
+      <button className="close-btn" onClick={closeSidebar}>
         <FaTimes />
       </button>
     </div>

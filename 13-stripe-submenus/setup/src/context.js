353c7686f02b55ec7923dc99d974a737bd2cb6f4
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-23 18:38:01 +0100

    add 2 open functions

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -6,6 +6,15 @@ const AppContext = React.createContext();
 const AppProvider = ({children}) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+    
+    const openModal = () => {
+        setIsModalOpen(true);
+    }
+
     return <AppContext.Provider>
         {children}
     </AppContext.Provider>

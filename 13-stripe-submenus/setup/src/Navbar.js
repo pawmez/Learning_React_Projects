565647a5ea959f532c4a2784bcff364266101c75
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-28 18:44:08 +0100

    close submenu when hover over navbar.js

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -14,7 +14,13 @@ const Navbar = () => {
     openSubmenu(page,{center,bottom});
   };
 
-  return <nav className="nav">
+  const handleSubmenu = (e) => {
+    if(!e.target.classList.contains('link-btn')){
+      closeSubmenu();
+    }
+  }
+
+  return <nav className="nav" onMouseOver={handleSubmenu}>
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} className="nav-logo" alt="stripe"/>

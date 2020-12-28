a11feb054487d03e5c18a97f6772980304ab3e9a
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-28 18:47:33 +0100

    delete onMouseOver from signin button

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
           </button>
         </li>        
       </ul>
-        <button className="btn signin-btn" onMouseOver={displaySubmenu}>
+        <button className="btn signin-btn">
             sign in
         </button>
     </div>

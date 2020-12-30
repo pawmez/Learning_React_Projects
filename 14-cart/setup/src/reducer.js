11f1ddb973a2aa4f787a7b963c99d5f4927cc688
Pawel Mezydlo <pmezydlo@gmail.com>
2020-12-30 19:47:02 +0100

    add INCREASE action type to reducer.js

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -13,7 +13,24 @@ const reducer = (state, action) => {
             cart: newCart
         }
     }
-        return state
+    if(action.type === 'INCREASE'){
+        let tempCart = state.cart.map((item) => {
+            if(item.id === action.payload){
+                return {
+                    ...item,
+                    amount: item.amount + 1
+                }
+            };
+            return item
+        })        
+        return {
+            ...state,
+            cart: tempCart
+        }        
+    }
+    return state
 }
+        
+
 
 export default reducer 

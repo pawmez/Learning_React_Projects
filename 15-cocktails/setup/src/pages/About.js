3d5d7d3ce4ca1474bebc1fd89264b1afa9501cd7
Pawel Mezydlo <pmezydlo@gmail.com>
2021-01-03 20:28:15 +0100

    structure About.js

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat sapiente eaque quis in, quam facere nobis temporibus praesentium ab neque repudiandae culpa quibusdam hic at reiciendis! Molestias ab ducimus, alias quas natus, dolores quam accusamus tempora quo praesentium ut unde!</p>
+    </section>
   )
 }
 

d07ed6af03e811775d11e4c9fc39da5e906c804c
Pawel Mezydlo <pmezydlo@gmail.com>
2021-01-09 11:15:27 +0100

    add fetchDrinks function; WIP

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -9,6 +9,29 @@ const [loading, setLoading] = useState(true);
 const [searchTerm, setSearchTerm] = useState('a');
 const [cocktails, setCocktails] = useState([]);
 
+const fetchDrinks = async () => {
+  setLoading(true);
+  try {
+    const response = await fetch(`${url}${searchTerm}`)
+    const data = await response.json();
+    const {drinks} = data;
+    if (drinks){
+
+
+    } else {
+      setCocktails([]);
+    }
+    setLoading(false)
+  } catch (err) {
+    console.log(err)
+    setLoading(false)
+  }
+}
+
+useEffect(() => {
+  fetchDrinks();
+}, [searchTerm])
+
   return <AppContext.Provider 
   value={{
     loading,    

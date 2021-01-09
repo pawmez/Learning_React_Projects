839db599aac0b9aa6c798885baac9ff4d98f1632
Pawel Mezydlo <pmezydlo@gmail.com>
2021-01-09 12:30:08 +0100

    add useEffect, setup fetching function

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -7,6 +7,21 @@ const SingleCocktail = () => {
   const {id} = useParams();
   const [loading, setLoading] = React.useState(false);
   const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    const getCocktail  = async () => {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+        setLoading(false)
+      }
+    }
+    getCocktail();
+  }, [id])
   
   return (
     <div>

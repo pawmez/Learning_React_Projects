34be9c23ae6eb9c593f24b20ae7026308720958c
Pawel Mezydlo <pmezydlo@gmail.com>
2021-01-09 12:39:36 +0100

    destructure the single cocktail API

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -15,6 +15,36 @@ const SingleCocktail = () => {
         const response = await fetch(`${url}${id}`);
         const data = await response.json();
         console.log(data);
+        //destructure the API and setup own key names
+        if(data.drinks){
+          const {
+            strDrink:name,
+            strDrinkThumb:image,
+            strAlcoholic:info,
+            strCategory:category,
+            strGlass:glass,
+            strInstructions:instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0]
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ]
+          const newCocktail = {
+            name,image,info,category,glass,instructions,ingredients
+          }
+          setCocktail(newCocktail)
+        } else {
+          setCocktail(null);
+        }
+        setLoading(false)
       } catch (err) {
         console.log(err);
         setLoading(false)
@@ -22,8 +52,11 @@ const SingleCocktail = () => {
     }
     getCocktail();
   }, [id])
+
+
   
   return (
+    
     <div>
       <h2>{id}</h2>
     </div>

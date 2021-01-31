0d4e569bcc21fdc47b9f6e744319a2ec48ec3c6b
Pawel Mezydlo <pmezydlo@gmail.com>
2021-01-31 17:49:56 +0100

    setup useStates, fetching function, useEffect

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -6,6 +6,27 @@ const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async() => {
+    setLoading(true);
+    let url;
+    url=`${mainUrl}${clientID}`
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchImages();    
+  }, [])
+
   return <h2>stock photos starter</h2>
 }
 

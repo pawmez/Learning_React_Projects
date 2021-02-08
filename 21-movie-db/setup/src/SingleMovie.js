73a22143049947b2d7bb7095583f958ec1548ead
Pawel Mezydlo <pmezydlo@gmail.com>
2021-02-08 22:19:53 +0100

    add state values, fetch function, useEffect to SingleMovie.js

--- 21-movie-db/setup/src/SingleMovie.js
+++ 21-movie-db/setup/src/SingleMovie.js
@@ -3,6 +3,22 @@ import { useParams, Link } from 'react-router-dom'
 import { API_ENDPOINT } from './context'
 
 const SingleMovie = () => {
+  const {id} = useParams();
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState({show:false,msg:''});
+
+const fetchMovie = async (url) => {
+  setLoading(true);
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
+
+useEffect(() => {
+  fetchMovie(`${API_ENDPOINT}&i=${id}`)
+}, [])
+
   return <h2>single movie</h2>
 }
 

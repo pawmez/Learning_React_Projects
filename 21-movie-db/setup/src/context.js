5373318c8dfa3668d85b4e038fdff3c2dc268dec
Pawel Mezydlo <pmezydlo@gmail.com>
2021-02-09 21:25:21 +0100

    clear useFetch.js code from context.js

--- 21-movie-db/setup/src/context.js
+++ 21-movie-db/setup/src/context.js
@@ -4,32 +4,8 @@ export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState({show:false,msg:''});
-  const [movies, setMovies] = useState([]);
+
   const [query, setQuery] = useState("hobbit");
-
-  const fetchMovies = async (url) => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-     
-      if (data.Response === "True") {
-        setMovies(data.Search)
-        setError({show:false,msg:''})
-      } else {
-        setError({show:true,msg:data.Error})
-      }
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`);
-  }, [query])
 
   return <AppContext.Provider value={{
     loading,

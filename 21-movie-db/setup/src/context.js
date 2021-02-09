9d4a0049f22e6be1bcea15c784bd729d297369a4
Pawel Mezydlo <pmezydlo@gmail.com>
2021-02-09 21:28:27 +0100

    import useFetch.js to context.js, destructure

--- 21-movie-db/setup/src/context.js
+++ 21-movie-db/setup/src/context.js
@@ -1,11 +1,14 @@
 import React, { useState, useContext, useEffect } from 'react'
+import useFetch from './useFetch'
+
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-
   const [query, setQuery] = useState("hobbit");
+  const {loading, error, data:movies} = useFetch(`&s=${query}`)
 
   return <AppContext.Provider value={{
     loading,

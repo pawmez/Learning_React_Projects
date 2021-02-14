e592cb061140ebe08b0643e821d0f3b2f01476aa
Pawel Mezydlo <pmezydlo@gmail.com>
2021-02-14 18:04:16 +0100

    destructure hits, start creating a return in Stories.js

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -3,12 +3,20 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-  const {isLoading} = useGlobalContext();
+  const {isLoading, hits} = useGlobalContext();
 
   if(isLoading) {
     return <div className="loading"></div>
   }
-  return <h2>stories component</h2>
+  return <section className="stories">
+    {hits.map((story)=>{
+      const {objectID:id,title,num_comments,url,points,author} = story
+      console.log(story);
+      return <article key={id} className="story">
+        <h4 className="title">{title}</h4>
+      </article>
+    })}
+  </section>
 }
 
 export default Stories

532b4a406d583be010e282ccb78c1f3dfe2f4de8
Pawel Mezydlo <pmezydlo@gmail.com>
2021-02-14 17:48:51 +0100

    add state values for the url, build an url

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -12,7 +12,11 @@ import reducer from './reducer'
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
 const initialState = {
-  isLoading:true
+  isLoading:true,
+  hits:[],
+  query:'react',
+  page:0,
+  nbPage:0
 }
 
 const AppContext = React.createContext()
@@ -22,10 +26,17 @@ const AppProvider = ({ children }) => {
 
   const fetchStories = async (url) => {
     dispatch({type:SET_LOADING})
+    try {
+      const response = await fetch(url)
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   useEffect(() => {
-    fetchStories()
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
   }, [])
 
   return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>

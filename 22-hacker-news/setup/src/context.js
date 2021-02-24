cb412490175e245ad1e1082b49b5b629fc0f83a6
Pawel Mezydlo <pmezydlo@gmail.com>
2021-02-24 22:40:28 +0100

    add handleSearch to context.js

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -39,11 +39,15 @@ const AppProvider = ({ children }) => {
     dispatch({type:REMOVE_STORY, payload:id})
   }
 
+  const handleSearch = (query) => {
+    dispatch({type:HANDLE_SEARCH,payload:query})
+  }
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
   }, [])
 
-  return <AppContext.Provider value={{...state, removeStory}}>{children}</AppContext.Provider>
+  return <AppContext.Provider value={{...state, removeStory, handleSearch}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
